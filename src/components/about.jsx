34092b87661859591cb68de07da279ae9857e331
Andrew <terminatordog0000@gmail.com>
2025-01-01 18:35:23 -0500

    Added about me details, as well as centered navbar, editted footer links, and added font to index.html

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,7 +5,42 @@ const About = () => {
 
     return(
         <div>
-            <h2 className="text">ABOUT ME!</h2>
+            <h4>
+                Ideas to do: make a side bar that has topics such as "professional life", "personal life / hobbies", "technical projects and interests", and "research".
+                Then a user can click one and the corresponding text from below will show. Alongside maybe an image or two to supplement it.
+            </h4>
+            <h2 className="text">About Me.</h2>
+            <p className="text">
+                Hello again! My name is Andrew Dickman, and I'm a 22 year old recent graduate from New Jersey Institute of Technology, who graduated in December 2024 with &nbsp;
+                <i>magna cum laude</i>&nbsp;honors. I love math and science, so I decided to start off my college career as an electrical engineering student, and studied EE for 
+                about two years until I decided it wasn't for me. I still loved the math and science, but realized I much preferred working with software, so at the end of my 
+                fourth semester, I switched to Computer Science. This was where I found my love for software development, and software engineering.
+                <br /> <br />
+                After finally building up the knowledge and confidence I needed, I was able to get an internship in the summer of 2024 at Johnson & Johnson where I worked as
+                a Data Scientist, primarily focused on Generative AI development and implementation, within their Technical Research & Development center. This experience
+                opened the door to Data Science for me, and showed me how important and diverse of a field it really is.
+                <br /> <br />
+                Because of my love and interest for software engineering, and my growing interest in Data Science, I'm looking for a role in either field to learn more about 
+                them professionally. I believe these roles should go hand in hand though, and a position which utilizes aspects of both would be an amazingly strong and
+                interesting position that I hope to work in some day.
+                <br /><br />
+                Outside of my professional life, I have a couple hobbies I like to enjoy when I have the time to. For example, I love to play card games, such as Magic The 
+                Gathering, Blackjack, Poker, as well as some roleplaying games, such as Dungeons & Dragons. I also enjoy playing video games, such as League of Legends, which
+                I played competitively for some time, and coding in my free time. When I'm outside, I love to spend time riding my motorcycle, and also playing paintball, 
+                hiking, or going on walks with my dog. I've also developed a deep interest in religion and history, specifically ancient religions, such as Ancient Christianity,
+                Ancient Judiasm, Zoroastrianism, and the early beginnings of other religions as well. 
+                <br /><br />
+                CODING AND PROJECTS, BUILDING WEBSITES, SKILLS W TOOLS AND LANGUAGES, DISCORD BOTS & DOCKER, WANTING TO BUILD CHESS ENGINE, ETC.
+                Since I am a Computer Science graduate, I think it's important to talk about my experiences coding, as well as projects and other things I've done or have
+                interests in.
+                <br /><br />
+                In addition to everything, I've also begun working with a professor from my college to conduct research within the field of Generative AI. I developed a strong 
+                interest in this field from my internship, and researching is helping me to grow that interest even more. To be specific, my primary interest area, as well as 
+                my research area, is specifically involved with the latent space for Large Language Models. I'll be looking for ways to try to understand this area better, but
+                also the focus will be on visualizing this space to make it easier to understand and interpret.
+                
+                This research is still in it's infancy stages though, and there's not much to show for it yet, as we just began with this idea.
+            </p>
         </div>
     );
 };
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -18,16 +18,16 @@ const SiteNavBar = ({currPage, setCurrPage, setPrevPage, darkMode, setDarkMode})
     return(
       <div>
         <Navbar bg={darkMode ? "dark" : "light"} expand="lg">
-          <Navbar.Brand style={styles.navBarItems}>My Website</Navbar.Brand>
+          <Navbar.Brand style={styles.navBarItems}>AJD</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
           <Nav className="mr-auto">
               <Nav.Link as={NavLink} to={"/home"} onClick={() => {setPrevPage(currPage); setCurrPage("Home");}} style={styles.navBarItems} className="navbar-links">Home</Nav.Link>
               <Nav.Link as={NavLink} to={"/about"} onClick={() => {setPrevPage(currPage); setCurrPage("About");}} style={styles.navBarItems}>About</Nav.Link>
               <Nav.Link as={NavLink} to={"/projects"} onClick={() => {setPrevPage(currPage); setCurrPage("Projects");}} style={styles.navBarItems}>Projects</Nav.Link>
+              <Nav.Link as={NavLink} to={"/research"} onClick={() => {setPrevPage(currPage); setCurrPage("Research");}} style={styles.navBarItems}>Research</Nav.Link>
               <Nav.Link as={NavLink} to={"/resume"} onClick={() => {setPrevPage(currPage); setCurrPage("Resume");}} style={styles.navBarItems}>Resume</Nav.Link>
               <Nav.Link as={NavLink} to={"/contact"} onClick={() => {setPrevPage(currPage); setCurrPage("Contact");}} style={styles.navBarItems}>Contact</Nav.Link>
-              <Nav.Link as={NavLink} to={"/research"} onClick={() => {setPrevPage(currPage); setCurrPage("Research");}} style={styles.navBarItems}>Research</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
