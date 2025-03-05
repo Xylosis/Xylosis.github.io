0239b78b06ff17b366d6ac18aa1c39adbf124e09
Andrew <terminatordog0000@gmail.com>
2025-03-04 20:41:49 -0500

    Updated from aspiring to working

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -33,7 +33,7 @@ const Home = ({currPage, setCurrPage, setPrevPage, darkMode, navIsOpen}) => {
             <ReactTypingEffect
                 text={[
                     "Hi, I'm Andrew Dickman...",
-                    "Aspiring Software Engineer / Data Scientist.",
+                    "Junior Software Engineer."//"Aspiring Software Engineer / Data Scientist.",
                 ]}
                 speed={100}
                 eraseSpeed={50}
@@ -50,7 +50,7 @@ const Home = ({currPage, setCurrPage, setPrevPage, darkMode, navIsOpen}) => {
                                         ? index > 7 && index < 22
                                             ? 'rgb(240, 0, 48)'
                                             : null
-                                        : index > 8 && index < 27
+                                        : index > /*8*/6 && index < 27
                                         ? 'rgb(26, 150, 30)'
                                         : index > 28 && index < 43
                                         ? 'rgb(76, 76, 231)'
@@ -76,7 +76,7 @@ const Home = ({currPage, setCurrPage, setPrevPage, darkMode, navIsOpen}) => {
                 }}
             />
 
-                <p className={"text subtitle"}>I'm a recent graduate from NJIT with a B.S. in Computer Science.<br />I'm currently looking for a position as a Software Engineer, or a Data Scientist.</p>
+                <p className={"text subtitle"}>I'm a recent graduate from NJIT with a B.S. in Computer Science.<br />I'm currently a Junior Software Engineer at ToaRe.</p>
             </div>
             </div>
             <NavLink as={NavLink} to={"/about"} onClick={() => {setPrevPage(currPage); setCurrPage("About"); const v = navIsOpen ? document.getElementsByClassName("navbar-toggler")[0].click() : null;}} className={classes}> Learn More About Me! </NavLink>
